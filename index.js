cf83c33af818a3eb118a61fd5a68f08a912e7460
Hayyanvora <hayyanvora786@gmail.com>
2023-01-16 23:21:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+//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
+//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
+
+const { Configuration, OpenAIApi } = require("openai");
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const configuration = new Configuration({
+    organization: "org-f4VUXXTGQd7sF9HHimnjyIPW",
+    apiKey: "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",
+});
+
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+const port = 3080
+app.use(bodyParser.json())
+app.use(cors());
+
+app.post('/', async (req, res) => {
+    if (req.body && req.body.message) {
+        const {message, currentModel} = req.body;
+        console.log(message, "message");
+        console.log(currentModel, "currentModel");
+        const response = await openai.createCompletion({
+            model: `${currentModel}`,
+            prompt: `${message}`,
+            max_tokens: 100,
+            temperature: 0.5,
+        });
+        res.json({
+            message: response.data.choices[0].text,
+        })
+    } else {
+        res.json({
+            data: 'message not present in the request body'
+        })
+    }
+});
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Server is running"
+    });
+});
+
+app.get('/models', async (req, res) => {
+    const response = await openai.listEngines();
+    console.log(response.data.data)
+    res.json({
+        models: response.data.data
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`)
+});
+
